1378bd0c1d70f05ef00ba6c5b7c629c42a7d2535
Arturs-gailitis <artursgailitis2003@gmail.com>
2025-06-28 13:01:37 +0300

    Create Drawing_eight.py

---
+++ Drawing_eight.py
@@ -0,0 +1,288 @@
+# 1. row
+# Loop to add spaces before the asterisk
+for i in range(12):
+    print(" ", end= " ")
+# Printing the asterisk count
+for i in range(1):
+    print("*", end= " ")
+# Printing new line that are empty
+print (" ")
+print (" ")
+
+# 2. row
+for i in range(12):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 3. row
+for i in range(12):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 4. row
+for i in range(9):
+    print(" ", end= " ")
+for i in range(7):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 5. row
+for i in range(7):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(9):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 6. row
+for i in range(5):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for f in range(2):                  
+    print(" ", end= " ")
+for i in range(8):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 7. row
+for i in range(4):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(10):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 8. row
+for i in range(3):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(4):
+    print("*", end= " ")
+for i in range(4):
+    print(" ", end= " ")
+for i in range(4):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 9. row
+for i in range(3):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(4):
+    print("*", end= " ")
+for i in range(4):
+    print(" ", end= " ")
+for i in range(4):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 10. row
+for i in range(4):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(10):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 11. row
+for i in range(1):
+    print(" ", end= " ")
+for i in range(3):
+    print("*", end= " ")
+for i in range(1):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(8):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(1):
+    print(" ", end= " ")
+for i in range(3):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 12. row
+for i in range(4):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(10):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 13. row
+for i in range(3):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(4):
+    print("*", end= " ")
+for i in range(4):
+    print(" ", end= " ")
+for i in range(4):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 14. row
+for i in range(3):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(4):
+    print("*", end= " ")
+for i in range(4):
+    print(" ", end= " ")
+for i in range(4):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 15. row
+for i in range(4):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(10):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 16. row
+for i in range(5):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(8):
+    print("*", end= " ")
+for i in range(2):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 17. row
+for i in range(7):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+for i in range(9):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 18. row
+for i in range(9):
+    print(" ", end= " ")
+for i in range(7):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 19. row
+for i in range(12):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 20. row
+for i in range(12):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
+print (" ")
+print (" ")
+
+# 21. row
+for i in range(12):
+    print(" ", end= " ")
+for i in range(1):
+    print("*", end= " ")
